008b0dcb726a17568a74be71da69c822ff435921
Kristoffer <ktis10@hotmail.com>
2020-10-28 10:26:30 +0100

    kar til at implementere i Spillet

--- src/main/java/com/example/galgelegafleveringen/Start.java
+++ src/main/java/com/example/galgelegafleveringen/Start.java
@@ -2,13 +2,22 @@ package com.example.galgelegafleveringen;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class Start extends AppCompatActivity {
-
+    Button b;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+
+        b = (Button) findViewById(R.id.start);
+        b.setOnClickListener(v -> StartSide());
+    }
+    public void StartSide(){
+        Intent intent = new Intent(this, Spillet.class);
+        startActivity(intent);
     }
 }
