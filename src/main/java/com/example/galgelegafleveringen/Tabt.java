7e2c801a64eab299dc34dbbda23be074a9b09034
Kristoffer <ktis10@hotmail.com>
2020-11-01 16:48:28 +0100

    Vind og tab fungere

--- src/main/java/com/example/galgelegafleveringen/Tabt.java
+++ src/main/java/com/example/galgelegafleveringen/Tabt.java
@@ -2,13 +2,33 @@ package com.example.galgelegafleveringen;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class Tabt extends AppCompatActivity {
+    String rigtigtOrd;
+    TextView TWRigtigtOrd;
+    Button BTPrøvIgen;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tabt);
+
+        rigtigtOrd = getIntent().getExtras().getString("orderet");
+        TWRigtigtOrd = findViewById(R.id.TWDetRigtigOrd);
+        BTPrøvIgen = findViewById(R.id.BTPrøvIgen);
+        TWRigtigtOrd.setText(rigtigtOrd);
+
+        BTPrøvIgen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Tabt.this, Start.class));
+                finish();
+            }
+        });
     }
 }
